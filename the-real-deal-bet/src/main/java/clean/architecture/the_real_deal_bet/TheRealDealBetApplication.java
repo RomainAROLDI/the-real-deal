ce115dbd480542ae1345c0010eb2066748ad26e9
Marianehrg <marianeaharag@gmail.com>
2024-11-19 10:39:08 +0100

    Restructuration of the repo

---
+++ the-real-deal-bet/src/main/java/clean/architecture/the_real_deal_bet/TheRealDealBetApplication.java
@@ -0,0 +1,13 @@
+package clean.architecture.the_real_deal_bet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheRealDealBetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheRealDealBetApplication.class, args);
+	}
+
+}
---
+++ the-real-deal-bet/src/test/java/clean/architecture/the_real_deal_bet/TheRealDealBetApplicationTests.java
@@ -0,0 +1,13 @@
+package clean.architecture.the_real_deal_bet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheRealDealBetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
