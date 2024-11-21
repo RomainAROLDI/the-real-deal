17b2251812e5f14eaaf04253c2a079a137fee5bf
kbrdn1 <kylianb1@icloud.com>
2024-11-21 15:31:10 +0100

    🚩 feat(api): update index

--- api-gateway/src/index.ts
+++ api-gateway/src/index.ts
@@ -1,9 +1,31 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { logger } from "hono/logger";
+import Controller from "./controller";
+import { getPort } from "./utils";
 
-const app = new Hono()
+const api = new Hono().basePath("/v1");
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+api.use(logger());
 
-export default app
+api.all("*", async (c) => {
+  const currentPath = c.req.path.replace("/v1/", "");
+  const split = currentPath.split("/");
+
+  const name = split[0] !== "api" ? split[0] : `${split[0]}/${split[1]}`;
+
+  // get rest after name in url
+  const baseUri = currentPath.replace(`${name}/`, "");
+
+  const controller = new Controller(name, {
+    port: getPort(name),
+    baseUri,
+    service: split[0] !== "api" ? `${name}:${currentPath.split("/")[1]}` : name,
+  });
+
+  return controller.fetch(c);
+});
+
+export default {
+  port: process.env.PORT!,
+  fetch: api.fetch,
+};
